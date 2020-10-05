c002acd0c2a064d3f15b7cc93d4e221ce6cf49fc
Michael Bailey <michaelbailey1@fb.com>
2020-10-05 15:35:21 +0100

    turned listen callback to async

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ let app = express()
 
 app.use(express.static("public"))
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
     console.log("Web server has started :)");
+
+    // init data from the data base
 })
